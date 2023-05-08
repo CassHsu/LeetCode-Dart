16966ac8310af635cfb455252268df3facb78470
CassHsu <cass.hsu@gmail.com>
2023-05-08 22:18:36 +0800

    Create 2652-01.dart

---
+++ 2652/2652-01.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int sumOfMultiples(int n) {
+      int ans = 0;
+
+      for (int i = 0; i <= n; i++) {
+          if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+              ans += i;
+          }
+      }
+
+      return ans;
+  }
+}
