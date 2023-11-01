b59a379d761cd56e398a89ffa1aa1745704e91cd
CassHsu <cass.hsu@gmail.com>
2023-11-01 21:14:56 +0800

    Create 2894-01.dart

---
+++ 2894/2894-01.dart
@@ -0,0 +1,16 @@
+class Solution {
+  int differenceOfSums(int n, int m) {
+      int s1 = 0;
+      int s2 = 0;
+
+      for (int i = 1; i <= n; i++) {
+          if (i % m == 0) {
+              s2 += i;
+          } else {
+              s1 += i;
+          }
+      }
+
+      return s1 - s2;
+  }
+}
